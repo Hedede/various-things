53c9e21e2a7750728328ed3d6d209f464f64889e
Hudd <haddayn@gmail.com>
2016-07-28 17:04:53 +0300

    Add hdf::Value to variant v1

---
+++ Prototypes/C++/Variant/v1/Value.h
@@ -0,0 +1,244 @@
+/*
+ * Copyright (C) 2014-2015  absurdworlds
+ * Copyright (C)      2015  Hedede <haddayn@gmail.com>
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef _aw_hdf_value_
+#define _aw_hdf_value_
+#include <cstring>
+
+#include <string>
+
+#include <aw/common/types.h>
+#include <aw/math/Vector2d.h>
+#include <aw/math/Vector3d.h>
+#include <aw/math/Vector4d.h>
+#include <aw/utility/string/as_string.h>
+#include <aw/math/as_string.h>
+
+#include <aw/hdf/Type.h>
+
+namespace aw {
+namespace hdf {
+//! Class for holding any HDF Value.
+class Value {
+public:
+	Value()
+	{
+	}
+
+	template<typename val_type>
+	Value(val_type v)
+		: holder()
+	{
+		holder.set<val_type>(v);
+	}
+
+	/*! Assignment operator. Copies content from another
+	 *  Value class, overwriting existing contents.
+	 */
+	Value& operator = (Value const& other)
+	{
+		holder = other.holder;
+		return *this;
+	}
+
+	/*!
+	 * Get value if types match.
+	 * \param v
+	 *     Reference to variable which will hold result.
+	 *     Will be modified only if types match
+	 * \return
+	 *     true on success, false on failure.
+	 */
+	template<typename val_type>
+	bool get(val_type& v) const
+	{
+		return holder.get(v);
+	}
+
+	template<typename val_type>
+	opt<val_type> get() const
+	{
+		val_type tmp;
+
+		if (holder.get(tmp))
+			return tmp;
+
+		return nullopt;
+	}
+
+	//! Returns type of currently held value
+	hdf::Type getType() const
+	{
+		return holder.type;
+	}
+
+	//! Set value if types are matching
+	template<typename val_type>
+	bool trySet(val_type const& v)
+	{
+		if(compareType(holder.type, v)) {
+			holder.set<val_type>(v);
+			return true;
+		}
+		return false;
+	}
+
+	//! Set value, resetting type
+	template<typename val_type>
+	void set(val_type const v)
+	{
+		holder.set<val_type>(v);
+	}
+
+	//! Reset value to <Unknown>
+	void reset()
+	{
+		holder.reset();
+	}
+
+private:
+	template <typename... Ts>
+	struct Helper;
+
+	template <typename T, typename... Ts>
+	struct Helper<T, Ts...> {
+		static void destroy(Type type, void* data)
+		{
+			if (checkType<T>(type))
+				reinterpret_cast<T*>(data)->~T();
+			else
+				Helper<Ts...>::destroy(type, data);
+		}
+		static void copy(Type type, void const* from, void* to)
+		{
+			if (checkType<T>(type))
+				new (to) T(*reinterpret_cast<T const*>(from));
+			else
+				Helper<Ts...>::copy(type, from, to);
+		}
+		static void move(Type type, void* from, void* to)
+		{
+			if (checkType<T>(type))
+				new (to) T(std::move(from));
+			else
+				Helper<Ts...>::move(type, from, to);
+		}
+	};
+
+
+	template <typename... Ts>
+	struct Holder {
+		static size_t const size = std::max({sizeof(Ts)...});
+		static size_t const align = std::max({alignof(Ts)...});
+
+		typedef typename std::aligned_storage<size, align>::type storage;
+		typedef Helper<Ts...> helper;
+
+		hdf::Type type;
+		storage data;
+
+		Holder()
+			: type(Type::Unknown)
+		{
+		}
+
+		template<typename T, typename... Args>
+		Holder(Args&&... args)
+		{
+			set<T>(std::forward<Args>(args)...);
+		}
+
+		Holder(Holder<Ts...> const& other)
+			: type(other.type)
+		{
+			helper::copy(other.type, &other.data, &data);
+		}
+
+		Holder(Holder<Ts...>&& other)
+			: type(other.type)
+		{
+			helper::move(other.type, &other.data, &data);
+		}
+
+		Holder<Ts...>& operator=(Holder<Ts...> const& other)
+		{
+			helper::destroy(type, &data);
+			helper::copy(other.type, &other.data, &data);
+			type = other.type;
+			return *this;
+		}
+
+		~Holder()
+		{
+			reset();
+		}
+
+		template<typename T, typename... Args>
+		void set(Args&&... args)
+		{
+			helper::destroy(type, &data);
+			new (&data) T(std::forward<Args>(args)...);
+			type = typeof<T>::value;	
+		}
+
+		template<typename T>
+		bool get(T& target) const
+		{
+			if (checkType<T>(type)) {
+				target = *reinterpret_cast<T const*>(&data);
+				return true;
+			}
+			return false;
+		}
+
+		void reset()
+		{
+			helper::destroy(type, &data);
+		}
+	};
+
+	typedef Holder<bool, i64, f64, std::string,
+	       Vector2d<f32>, Vector3d<f32>, Vector4d<f32>> holder_t;
+	holder_t holder;
+};
+
+template <>
+struct Value::Helper<> {
+	static void destroy(Type type, void* data) { }
+	static void copy(Type type, void const* from, void* to) { }
+	static void move(Type type, void* from, void* to) { }
+};
+} // namespace hdf
+
+//! Convert Value to string
+inline std::string as_string(hdf::Value const& val)
+{
+	switch (val.getType()) {
+	case hdf::Type::String:
+		return as_string(val.get<std::string>());
+	case hdf::Type::Float:
+		return as_string(val.get<f64>());
+	case hdf::Type::Integer:
+		return as_string(val.get<i64>());
+	case hdf::Type::Boolean:
+		return as_string(val.get<bool>());
+	case hdf::Type::Vector2d:
+		return as_string(val.get<Vector2d<f32>>());
+	case hdf::Type::Vector3d:
+		return as_string(val.get<Vector3d<f32>>());
+	case hdf::Type::Vector4d:
+		return as_string(val.get<Vector4d<f32>>());
+	default:
+		break;
+	}
+
+	return "null";
+}
+} // namespace aw
+#endif//_aw_hdf_value_
