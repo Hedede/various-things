7d45c26696757a8c89c5c9c4d4b4621bf37ffa69
Hudd <hededrk@gmail.com>
2016-09-01 03:22:09 +0300

    CmdHist: Add prototype completion

--- Prototypes/C++/CmdHist/CmdHist.cpp
+++ Prototypes/C++/CmdHist/CmdHist.cpp
@@ -1,10 +1,21 @@
-#include <iostream>
+#include <set>
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <iostream>
+#include <aw/utility/algorithm.h>
 
 int main()
 {
+	std::set<std::string> commands;
+
+	commands.insert("set texture quality");
+	commands.insert("set text");
+	commands.insert("set time");
+	commands.insert("set gravity");
+	commands.insert("test audio");
+	commands.insert("test video");
+
 	std::vector<std::string> history;
 
 	std::string str;
@@ -16,5 +27,27 @@ int main()
 		std::cout << "-------------------\n";
 		for (auto const& s : history)
 			std::cout << ">" << s << "\n";
+
+		std::vector<std::string> matches;
+		auto it = commands.lower_bound(str);
+
+		while ( (it != end(commands)) && (it->find(str) == 0) )
+			matches.push_back(*it++);
+
+		if (matches.empty())
+			continue;
+
+		if (matches.size() == 1 && matches[0] != str) {
+			std::cout << "hint: " << matches[0] << "\n";
+			continue;
+		}
+
+		size_t prefix = aw::common_prefix_length(
+			begin(matches), end(matches), str.size());
+
+		if (prefix == str.size())
+			continue;
+
+		std::cout << "hint: " << matches[0].substr(0, prefix) << "\n";
 	}
 }
