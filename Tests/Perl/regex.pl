793e186665b89685944cf00967f15a997eb6ac29
Hudd <haddayn@gmail.com>
2016-10-20 22:35:15 +0300

    Tests/Perl: Add regex.pl
    Working with unicode in perl5 is frustrating.

---
+++ Tests/Perl/regex.pl
@@ -0,0 +1,5 @@
+print ("дерп обнови соундрепорт" =~ /(с[оа]+ундрепорт)/);
+print ("дерп обнови саундрепорт" =~ /(саундрепорт)/);
+print ("дерп обнови соундрепорт" =~ /(соундрепорт)/);
+print ("drane soundreport" =~ /(s[oa]undreport)/);
+print ("drane saundreport" =~ /(s[oa]undreport)/);
