bbff68bedb7270699573a4db04d7d28f79a85314
Hudd <haddayn@gmail.com>
2016-11-17 12:35:25 +0300

    CmdHist: Add another completion prototype.
    (I think it is GDB-style completion)

--- Prototypes/C++/CmdHist/CmdHist.cpp
+++ Prototypes/C++/CmdHist/CmdHist.cpp
@@ -5,6 +5,13 @@
 #include <iostream>
 #include <aw/utility/algorithm.h>
 
+struct Compare {
+	bool operator() (std::string const& a, std::string const& b) {
+		size_t const N = std::min(a.size(), b.size());
+		return a.substr(0,N) < b.substr(0, N);
+	}
+} compare;
+
 int main()
 {
 	std::set<std::string> commands;
@@ -18,6 +25,37 @@ int main()
 
 	std::vector<std::string> history;
 
+	auto find = [&] (std::string const& cmd) {
+	
+		unsigned i = 0;
+
+		auto pos = cmd.find(' ');
+		auto prefix = cmd.substr(0, pos);
+
+		auto r = std::equal_range(commands.begin(), commands.end(), prefix, compare);
+		auto r2 = r;
+		while (r2.first != r.second) {
+			++i;
+			if (pos == std::string::npos)
+				break;
+			pos = cmd.find(' ', pos+1);
+			prefix = cmd.substr(0, pos);
+			r = r2;
+			r2 = std::equal_range(r.first, r.second, prefix, compare);
+		}
+
+		std::cout << i << '\n';
+
+		if (std::distance(r2.first, r2.second) != 1)
+			return commands.end();
+
+		if (r2.first != commands.end())
+			if (*r2.first != prefix)
+				return commands.end();
+
+		return r2.first;
+	};
+
 	std::string str;
 	while ( std::getline(std::cin, str, '\n') ) {
 		auto pos = std::remove( begin(history), end(history), str );
@@ -26,7 +64,12 @@ int main()
 
 		std::cout << "-------------------\n";
 		for (auto const& s : history)
-			std::cout << ">" << s << "\n";
+			std::cout << "^" << s << "\n";
+
+		std::cout << ">" << str << '\n';
+		auto cmd = find(str);
+		if (cmd != commands.end())
+			std::cout << "!" << *cmd << "\n";
 
 		std::vector<std::string> matches;
 		auto it = commands.lower_bound(str);
