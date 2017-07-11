2396297856373c2bfdcc58e38db027fd80c11ca3
Hudd <haddayn@gmail.com>
2017-07-11 06:14:54 +0300

    Lua: Improve to_snake
    Handles some names with numbers in them better

--- Other/Lua/tosnake.lua
+++ Other/Lua/tosnake.lua
@@ -1,11 +1,24 @@
 function to_snake(name)
-	local CAPSInCamel = "(.)([A-Z][a-z]+)";
-	local CamelCase   = "([a-z0-9])([A-Z])";
-	return name:gsub(CAPSInCamel,"%1_%2"):gsub(CamelCase, "%1_%2"):lower();
+	local patterns = {
+		"(.)([A-Z][a-z]+)",
+		"([a-z])([0-9][A-Z])",
+		"([a-z])([A-Z])"
+	};
+
+	for _,pattern in ipairs(patterns) do
+		name = name:gsub(pattern, "%1_%2");
+	end
+
+	return name:lower();
 end
 
 print(to_snake("getSomeSnacks"));
 print(to_snake("aZ"));
+print(to_snake("Vec3"));
 print(to_snake('HDFParser'));
 print(to_snake('HDFToXML'));
 print(to_snake('LoadLibraryW'));
+print(to_snake('glTexImage3D'));
+print(to_snake('Int32ToBytes'));
+print(to_snake('BytesFromInt32'));
+
